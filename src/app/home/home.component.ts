fb193936b3fb36c91e238558dd8afecef3b306df
QaziOng <167695401+QaziOng@users.noreply.github.com>
2025-07-25 10:14:55 -0700

    same changes as beore

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SignupComponent } from '../components/signup/signup.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SignupComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
